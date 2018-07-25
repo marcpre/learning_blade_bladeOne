c6ee42b3b164a27427f9c98e7f1de2ada8a4637e
Ronniel <piscoster@gmail.com>
2018-07-25 18:10:13 +0000

    create template

--- views/gpuContent.blade.php
+++ views/gpuContent.blade.php
@@ -1,4 +1,40 @@
-Graphic Card: 
-----------------------------------------------------------------------------
-{{$price}}
-{{$id}}
+The Ultimative Guide for ALL {{$company}}-Miners with Profitability Estimates
+------------------------------------------------------------------------------
+{{$company}} has currently {{$numberOfMiners}} on the Market. The 
+
+
+
+
+
+<h2>How Profitability Will The Miners Be?</h2>
+
+The miners from {{$company}} are quite profitable. The reason for this is that the are focusing on the {{$algorithm}}, which is extremely robust. Furthermore, {{$company}} upgrades their miners on an ongoing basis and adjusts them to the market. 
+
+Find below an overview of our profitability estimates. All calculations are updated in real-time and take the current BTC price of {{$btcPrice}} into account.
+
+
+<h1><b>TABLE OF PROFITABILITIES</b></h1>
+
+The above estimates are quite conservative and we do not take any further overclocking into account, which can have tremendous effects on a {{$typeOfMiner}}. We expect the miners to deliver higher performance and efficiency when run in production.
+
+<h2>Which has the shortest payback period?</h2>
+
+...
+
+<h2>Which has the shortest payback period?</h2>
+
+We define payback period as the time that it takes to make your money back. As crypto markets have been proven in the past to be unpredictable the starting point that we can use is the current price of BTC. Hence, if you buy f.ex.: {{$randomSingleMiner}} today you would make your money back in {{$randomSingleMiner_days}} days.
+
+You can find below a list of the miners that have the shortest payback time:
+
+<h1><b> TABLE OF MINERS WITH PAYBACK TIME</b></h1>
+
+
+...
+
+<h1>Conclusion</h1>
+
+
+<h2>Sources:</h2>
+
+* ...
