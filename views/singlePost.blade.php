57d24277c3c83fda2574e5c1f12b98e0c6445eb5
marcpre <marcus.presich@gmail.com>
2018-08-05 19:33:44 +0200

    singlePost Template

--- views/singlePost.blade.php
+++ views/singlePost.blade.php
@@ -1,6 +1,4 @@
-<h1><b>General</b></h1>
-
-The Model {{miningModel}} from {{company}} was previously under the top 3 miners on coindation.com. It is mining Blake2B algorithm with a maximum hashrate of 4.3Th/s for a power consumption of 1350W.
+Nowadays you can mine Equihash coins using not only GPU but also ASIC miners. The most popular models are Antminer Z9 by the Chinese manufacturer Bitmain and the newly designed Innosilicon A9 ZMaster.<p></p><p>{{$company}}&nbsp;is still the on of the <synonym words="world most well known mining rig building companies, ">world premier ASIC manufacture</synonym><synonym words="world most well known mining rig building companies, "></synonym>, and it’s no secret that their <synonym words="mining rigs, rigs, computer hardware">hardware&nbsp;&nbsp;</synonym><synonym words="is used widely across the BTC network, that many miners use their hardware">powers most of the Bitcoin network</synonym>. If you want to mine <synonym words="BTC, the Bitcoin cryptocurrency, the most well known coin - BTC -">Bitcoin</synonym>, you are better off buying <synonym words="computer hardware, mining rigs, mining hardware">hardware</synonym> from {{$company}}. The <synonym words="company, manufacturer, mining rig builder, cooperation">firm</synonym>&nbsp;<synonym words="brought to the market, is selling">launched </synonym>the {{$model}}, their <synonym words="latest computer hardware">latest miner</synonym><synonym words=""></synonym>.</p><p>The&nbsp;@if( exists(category) )&nbsp; "{{ $category }}" @endif&nbsp;<synonym words="miner">model</synonym> {{miningModel}} from {{company}} was previously under the <synonym words="top, best, most profitable">top 3</synonym> miners on coindation.com. It is mining {{$algorithm}} algorithm with a <synonym words=" , max">maximum </synonym>hashrate of {{ $hashRate }} <synonym words="and a power consumption of, consuming power as much as">for a power consumption of</synonym> {{ $powerConsumption }}.
 
 @if (count($listOfAlgorithms) === 1)
     The model focuses on the following algorithms: {{listOfAlgorithms}}. 
@@ -8,8 +6,13 @@ The Model {{miningModel}} from {{company}} was previously under the top 3 miners
     On the one hand the {{$model}} supports the algorithm {{listOfAlgorithms}} and on the other hand you can buy miner that support {{listOfAlgorithms}}. 
 @else
    {{$Model}} only supports one single algorithm,  {{listOfAlgorithms}}
-@endif
+@endif You can mine with the&nbsp;{{$model}} the <synonym words=" ">following </synonym><synonym words="crypto-coins, cryptocurrencies">coins</synonym>:&nbsp;{{ $listOfCryptocurrencies }}</p><p>
 
+</p><h1><synonym words=" Pros/Cons, Plus/Minuses, Like/dislike">Advantages/Disadvantages</synonym></h1>
+
+
+
+<br>
 <h1><b>Costs</b></h1>
 We assume an overall price per waconsttt of 0.1. Hence we derive a 
 
@@ -55,3 +58,4 @@ As you can see above as of today - {{$today}} - the current mines are highly pro
 <h2>Sources:</h2>
 
 * ...
+<p></p>
