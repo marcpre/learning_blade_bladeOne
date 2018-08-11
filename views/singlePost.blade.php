b9c009ef9dd902e7973be18811be5c873a959af9
marcpre <marcus.presich@gmail.com>
2018-08-11 12:52:08 +0200

    mining template

--- views/singlePost.blade.php
+++ views/singlePost.blade.php
@@ -1,4 +1,4 @@
-Nowadays you can mine Equihash coins using not only GPU but also ASIC miners. The most popular models are Antminer Z9 by the Chinese manufacturer Bitmain and the newly designed Innosilicon A9 ZMaster.<p></p><p>{{$company}}&nbsp;is still the on of the <synonym words="world most well known mining rig building companies, ">world premier ASIC manufacture</synonym><synonym words="world most well known mining rig building companies, "></synonym>, and it’s no secret that their <synonym words="mining rigs, rigs, computer hardware">hardware&nbsp;&nbsp;</synonym><synonym words="is used widely across the BTC network, that many miners use their hardware">powers most of the Bitcoin network</synonym>. If you want to mine <synonym words="BTC, the Bitcoin cryptocurrency, the most well known coin - BTC -">Bitcoin</synonym>, you are better off buying <synonym words="computer hardware, mining rigs, mining hardware">hardware</synonym> from {{$company}}. The <synonym words="company, manufacturer, mining rig builder, cooperation">firm</synonym>&nbsp;<synonym words="brought to the market, is selling">launched </synonym>the {{$model}}, their <synonym words="latest computer hardware">latest miner</synonym><synonym words=""></synonym>.</p><p>The&nbsp;@if( exists(category) )&nbsp; "{{ $category }}" @endif&nbsp;<synonym words="miner">model</synonym> {{miningModel}} from {{company}} was previously under the <synonym words="top, best, most profitable">top 3</synonym> miners on coindation.com. It is mining {{$algorithm}} algorithm with a <synonym words=" , max">maximum </synonym>hashrate of {{ $hashRate }} <synonym words="and a power consumption of, consuming power as much as">for a power consumption of</synonym> {{ $powerConsumption }}.
+<p><p>Nowadays you can mine Equihash coins using not only GPU but also ASIC miners. The most popular models are Antminer Z9 by the Chinese manufacturer Bitmain and the newly designed Innosilicon A9 ZMaster.</p><p></p><p>{{$company}}&nbsp;is still the on of the <synonym words="world most well known mining rig building companies, ">world premier ASIC manufacture</synonym><synonym words="world most well known mining rig building companies, "></synonym>, and it’s no secret that their <synonym words="mining rigs, rigs, computer hardware">hardware&nbsp;&nbsp;</synonym><synonym words="is used widely across the BTC network, that many miners use their hardware">powers most of the Bitcoin network</synonym>. If you want to mine <synonym words="BTC, the Bitcoin cryptocurrency, the most well known coin - BTC -">Bitcoin</synonym>, you are better off buying <synonym words="computer hardware, mining rigs, mining hardware">hardware</synonym> from {{$company}}. The <synonym words="company, manufacturer, mining rig builder, cooperation">firm</synonym>&nbsp;<synonym words="brought to the market, is selling">launched </synonym>the {{$model}}, their <synonym words="latest computer hardware">latest miner</synonym><synonym words=""></synonym>.</p><p>The&nbsp;@if( exists(category) )&nbsp; "{{ $category }}" @endif&nbsp;<synonym words="miner">model</synonym> {{miningModel}} from {{company}} was previously under the <synonym words="top, best, most profitable">top 3</synonym> miners on coindation.com. It is mining {{$algorithm}} algorithm with a <synonym words=" , max">maximum </synonym>hashrate of {{ $hashRate }} <synonym words="and a power consumption of, consuming power as much as">for a power consumption of</synonym> {{ $powerConsumption }}.
 
 @if (count($listOfAlgorithms) === 1)
     The model focuses on the following algorithms: {{listOfAlgorithms}}. 
@@ -9,53 +9,8 @@ Nowadays you can mine Equihash coins using not only GPU but also ASIC miners. Th
 @endif You can mine with the&nbsp;{{$model}} the <synonym words=" ">following </synonym><synonym words="crypto-coins, cryptocurrencies">coins</synonym>:&nbsp;{{ $listOfCryptocurrencies }}</p><p>
 
 </p><h1><synonym words=" Pros/Cons, Plus/Minuses, Like/dislike">Advantages/Disadvantages</synonym></h1>
+<ul><li>Insert Randomly here advantages/disadvantages</li></ul>
 
 
-
-<br>
-<h1><b>Costs</b></h1>
-We assume an overall price per waconsttt of 0.1. Hence we derive a 
-
-
-###################
-
-The algorithm that the miner uses is an effective indicator to show how profitable certain coins are going to be.
-
-Find below an overview table, which coins are supported. Please note that this is a current selection as of {{$today}}:
-
-<h1><b>Overview of algorithm and the coins that it supports</b></h1>
-
-<h2>How Profitability Will The Miners Be?</h2>
-
-The miners from {{$company}} are quite profitable. The reason for this is that the are focusing on the {{$algorithm}}, which is extremely robust. Furthermore, {{$company}} upgrades their miners on an ongoing basis and adjusts them to the market. 
-
-Find below an overview of our profitability estimates. All calculations are updated in real-time and take the current BTC price of {{$btcPrice}} into account.
-
-
-<h1><b>TABLE OF PROFITABILITIES</b></h1>
-
-The above estimates are quite conservative and we do not take any further overclocking into account, which can have tremendous effects on a {{$typeOfMiner}}. We expect the miners to deliver higher performance and efficiency when run in production.
-
-<h2>Which has the shortest payback period?</h2>
-
-...
-
-<h2>Which has the shortest payback period?</h2>
-
-We define payback period as the time that it takes to make your money back. As crypto markets have been proven in the past to be unpredictable the starting point that we can use is the current price of BTC. Hence, if you buy f.ex.: {{$randomSingleMiner}} today you would make your money back in {{$randomSingleMiner_days}} days.
-
-You can find below a list of the miners that have the shortest payback time:
-
-<h1><b> TABLE OF MINERS WITH PAYBACK TIME</b></h1>
-
-If you buy miner {{$miner with the highest payback time}} today, at the current price of Bitcoin - BTC = {{$btcPrice}}, you will make your money back in {{$days}} with a daily profit of {{$dailyProfitOfMiner}}.
-
-...
-
-<h1>Conclusion</h1>
-As you can see above as of today - {{$today}} - the current mines are highly profitable. Find below a detailed 
-
-<h2>Sources:</h2>
-
-* ...
-<p></p>
+<h1><synonym words="Expenses, Electricity Costs, Operating Costs, Operating Expenses, Costs for Electricity, Costs for Operating the Rig">Costs</synonym><b></b></h1>In our profitability calculations we assume an overall price of 0.1. Hence we derive a cost base line of&nbsp;<insertdata>{{ $miningCosts }}</insertdata>&nbsp;. Hence the miner has&nbsp;<insertdata>{{$dailyProfitOfMiner}}</insertdata>&nbsp;until the miner gets profitable. (as per <insertdata>{{$today}}</insertdata>). <synonym words="We define 'payback period' as:, 'Payback Period' is defined for us as:, The definition of payback period is:">Our definition of payback period is</synonym>: <synonym words="Number of days to make your initial costs back.">The time that it takes the cryptominer to make its money back in days</synonym>. As crypto markets have been proven in the past to be <synonym words="quite volatile, fluctuating">unpredictable</synonym> we can only use as current starting point the <synonym words="price of BTC., the price of BTC.">current price of BTC.</synonym></p><p><p></p>
+<p></p></p>
